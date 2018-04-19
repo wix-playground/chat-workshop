9b72e0f2f654139d182aaa5b89ab54bac6a5d545
Martynas Bardauskas <martynasb@wix.com>
2018-04-19 16:58:37 +0300

    remove ws implementation from client

--- chat-client/App.js
+++ chat-client/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { WS_HOST, WS_PORT } from './config'
 import getWebSocketClient from 'wix-chat-workshop-server/client';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {message: 'before connection'}
-  }
-
-  componentDidMount() {
-    const ws = getWebSocketClient()
-    ws.connect(WS_HOST, WS_PORT, 'martynas', 'whoa').then(() => this.setState({message: 'connected'}))
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Init screen of chat client app!</Text>
-        <Text>ws status: {this.state.message}</Text>
       </View>
     );
   }
